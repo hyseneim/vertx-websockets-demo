8c3d6f8fb07d466b2b412d26eeb5cdd3d846883e
Gabriele Cipolloni <gabri9501@gmail.com>
2017-07-21 10:08:46 +0200

    Show server port.

--- src/main/java/it/hysen/vertx/websockets/demo/starter/AuctionServiceVerticle.java
+++ src/main/java/it/hysen/vertx/websockets/demo/starter/AuctionServiceVerticle.java
@@ -17,31 +17,31 @@ import it.hysen.vertx.websockets.demo.validator.AuctionValidator;
 import it.hysen.vertx.websockets.demo.web.AuctionHandler;
 
 public class AuctionServiceVerticle extends AbstractVerticle {
-	
-	public static final Logger _logger = LoggerFactory.getLogger(AuctionServiceVerticle.class);
 
+	public static final Logger _logger = LoggerFactory.getLogger(AuctionServiceVerticle.class);
+	
 	private Router auctionApiRouter() {
 		final AuctionRepository repository = new AuctionRepository(vertx.sharedData());
 		final AuctionValidator validator = new AuctionValidator(repository);
 		final AuctionHandler handler = new AuctionHandler(repository, validator);
-		
+
 		final Router router = Router.router(vertx);
 		router.route().handler(BodyHandler.create());
-		
+
 		router.route().consumes("application/json");
 		router.route().produces("application/json");
-
+		
 		router.route("/auctions/:id").handler(handler::initAuctionInSharedData);
 		router.get("/auctions/:id").handler(handler::handleGetAuction);
 		router.patch("/auctions/:id").handler(handler::handleChangeAuctionPrice);
-		
+
 		return router;
 	}
-
+	
 	private ErrorHandler errorHandler() {
 		return ErrorHandler.create();
 	}
-	
+
 	private SockJSHandler eventBusHandler() {
 		final BridgeOptions options = new BridgeOptions().addOutboundPermitted(new PermittedOptions().setAddressRegex("auction\\.[0-9]+"));
 		return SockJSHandler.create(vertx).bridge(options, event -> {
@@ -56,23 +56,33 @@ public class AuctionServiceVerticle extends AbstractVerticle {
 			event.complete(true);
 		});
 	}
-
+	
 	@Override
 	public void start(Future<Void> startFuture) {
 		final Router router = Router.router(vertx);
-		
+
 		router.route("/eventbus/*").handler(eventBusHandler());
 		router.mountSubRouter("/api", auctionApiRouter());
 		router.route().failureHandler(errorHandler());
 		router.route().handler(staticHandler());
-		
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 
-		startFuture.complete();
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080, listenHandler -> {
+			if (listenHandler.succeeded()) {
+				_logger.debug("Server started on port: " + listenHandler.result().actualPort());
+				
+				startFuture.complete();
+			}
+			else {
+				final Throwable throwable = listenHandler.cause();
+				_logger.error(throwable);
+				
+				startFuture.fail(throwable);
+			}
+		});
 	}
-
+	
 	private StaticHandler staticHandler() {
 		return StaticHandler.create().setCachingEnabled(false);
 	}
-	
+
 }
--- src/main/java/it/hysen/vertx/websockets/demo/starter/Runner.java
+++ src/main/java/it/hysen/vertx/websockets/demo/starter/Runner.java
@@ -5,19 +5,19 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
 public class Runner {
-	
+
 	public static final Logger _logger = LoggerFactory.getLogger(Runner.class);
-	
+
 	public static void main(String[] args) {
 		final Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(AuctionServiceVerticle.class.getName(), resultHandler -> {
 			if (resultHandler.succeeded()) {
-				_logger.debug("Server started.");
+				_logger.info("All verticles started.");
 			}
 			else {
 				_logger.error(resultHandler.cause());
 			}
 		});
 	}
-	
+
 }
